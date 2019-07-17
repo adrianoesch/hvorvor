43ee2169112569d8611c45ddbbbecec8ea4d524d
Oesch <adrian.oesch@schibsted.com>
2019-07-17 20:26:42 +0200

    fix deploy

--- manage.sh
+++ manage.sh
@@ -2,14 +2,13 @@
 
 case "$1" in
   start)
-    echo -n "Starting: "
+    echo -n "Starting Server: "
     cd /home/ec2-user/hvorvor
-    source ./env.txt
     gunicorn --workers 2 --bind localhost:5000 --daemon --log-file gunicorn.log wsgi:app
     echo "Success"
     ;;
   stop)
-    echo -n "Stopping SDL: "
+    echo -n "Stopping Server: "
     pkill gunicorn
     echo "Success"
     ;;
@@ -23,7 +22,9 @@ case "$1" in
     # Re-run this script with stop and start arguments.
     $0 stop
     sleep 2
+    echo -n "Git pull: "
     git pull
+    echo "Success"
     $0 start
     ;;
   reload|force-reload)
